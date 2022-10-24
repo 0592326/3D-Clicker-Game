31830921d3ad8db783be1e7e44931f7915927197
0537982 <87291146+0537982@users.noreply.github.com>
2022-10-25 10:30:45 +1100

    Updated in game 2D and 3D style, began multiplayer implementation, began tutorial popup
    :D

--- Main_Menu.gd
+++ Main_Menu.gd
@@ -3,12 +3,7 @@ extends Control
 var start_menu
 var level_select_menu
 var options_menu
-var lose_menu
-var lose_menu_2
-var win_menu
-var win_menu_2
-var end_menu
-var end_menu_2
+var multiplayer_menu
 
 export (String, FILE) var testing_area_scene
 export (String, FILE) var space_level_scene
@@ -19,12 +14,7 @@ func _ready():
 	start_menu = $Start_Menu
 	level_select_menu = $Level_Select_Menu
 	options_menu = $Options_Menu
-	#lose_menu = $Lose_Menu
-	#lose_menu_2 = $Lose_Menu_2
-	#win_menu = $Win_Menu
-	#win_menu_2 = $Win_Menu_2
-	#end_menu = $End_Menu
-	#end_menu_2 = $End_Menu_2
+	multiplayer_menu = $Multiplayer_Menu
 
 	$Start_Menu/Button_Start.connect("pressed", self, "start_menu_button_pressed", ["start"])
 	$Start_Menu/Button_Open_Godot.connect("pressed", self, "start_menu_button_pressed", ["open_godot"])
@@ -32,24 +22,15 @@ func _ready():
 	$Start_Menu/Button_Quit.connect("pressed", self, "start_menu_button_pressed", ["quit"])
 
 	$Level_Select_Menu/Button_Back.connect("pressed", self, "level_select_menu_button_pressed", ["back"])
-	$Level_Select_Menu/Button_Level_Testing_Area.connect("pressed", self, "level_select_menu_button_pressed", ["testing_scene"])
-	$Level_Select_Menu/Button_Level_Space.connect("pressed", self, "level_select_menu_button_pressed", ["space_level"])
-	$Level_Select_Menu/Button_Level_Ruins.connect("pressed", self, "level_select_menu_button_pressed", ["ruins_level"])
 	$Level_Select_Menu/Button_Level_Game.connect("pressed", self, "level_select_menu_button_pressed", ["game_level"])
+
+	$Multiplayer_Menu/Button_Back.connect("pressed", self, "multiplayer_menu_button_pressed", ["back"])
 
 	$Options_Menu/Button_Back.connect("pressed", self, "options_menu_button_pressed", ["back"])
 	$Options_Menu/Button_Fullscreen.connect("pressed", self, "options_menu_button_pressed", ["fullscreen"])
 	$Options_Menu/Check_Button_VSync.connect("pressed", self, "options_menu_button_pressed", ["vsync"])
 	$Options_Menu/Check_Button_Debug.connect("pressed", self, "options_menu_button_pressed", ["debug"])
 
-	#$Lose_Menu/Button_Main_Menu.connect("pressed", self, "lose_menu_button_pressed", ["menu"])
-	#$Lose_Menu_2/Button_Level_Select.connect("pressed", self, "lose_menu_button_pressed", ["level select"])
-
-	#$Win_Menu/Button_Main_Menu.connect("pressed", self, "win_menu_button_pressed", ["menu"])
-	#$Win_Menu_2/Button_Level_Select.connect("pressed", self, "win_menu_2_button_pressed", ["level select"])
-
-	#$End_Menu/Button_Main_Menu.connect("pressed", self, "end_menu_button_pressed", ["menu"])
-	#$End_Menu/Button_Main_Menu.connect("pressed", self, "end_menu_button_pressed", ["quit"])
 
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
